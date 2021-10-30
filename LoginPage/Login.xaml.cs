7b5e72bdaf4ca01e9dd57417c8cc93dace2ca72f
Антон Ахтямов <yznikfortyni@gmail.com>
2021-10-30 17:12:49 +0300

    Login.cs

--- LoginPage/Login.xaml.cs
+++ LoginPage/Login.xaml.cs
@@ -14,14 +14,35 @@ using System.Windows.Shapes;
 
 namespace LoginPage
 {
-    /// <summary>
-    /// Логика взаимодействия для Login.xaml
-    /// </summary>
+   
     public partial class Login : Window
     {
+        public static ado.Users users {get; set;}
         public Login()
         {
             InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            users = class_0.db_connection.connection.Users.Where(z => z.login == txt_login.Text && z.password == txt_password.Password).FirstOrDefault();
+            if (users != null)
+            {
+                MessageBox.Show($"авторизация успешна {users_name}");
+            }
         }
     }
 }
