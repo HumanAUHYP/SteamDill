49f8728b4ded70eed54a4f04d365e8054372abe8
DellPK <humanplay228@gmail.com>
2021-10-30 17:03:49 +0300

    db connection

---
+++ LoginPage/db_connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage
+{
+    class db_connection
+    {
+        public static SteamDillEntities connection = new SteamDillEntities();
+    }
+}
